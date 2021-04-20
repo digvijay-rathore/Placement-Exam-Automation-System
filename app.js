f00d18d27a2de7837307f1efd7d8c3f69c7d0c43
Digvijay Rathore <digvijaysjodha@gmail.com>
2021-04-20 19:48:18 +0530

    Added authentication

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ const express = require('express');
 const path = require('path');
 const jade = require('jade');
 const bodyParser = require('body-parser');
+const passport = require('passport');
+
+const flash = require('connect-flash');
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
 
 const app = express();
 
@@ -12,19 +17,27 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(bodyParser.urlencoded({extended:false}));
 
+require('./config/passport')(passport);
+
+app.use(cookieParser());
+app.use(session({secret: 'my_app_secret'}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+
 var index = require('./controllers/index');
 var students = require('./controllers/student');
 var faculty = require('./controllers/faculties');
 var courses = require('./controllers/courses');
 var admin = require('./controllers/admin');
+var login = require('./controllers/login_controller');
 
 app.use('/', index);
 app.use('/students', students);
 app.use('/faculties', faculty);
 app.use('/courses', courses);
 app.use('/admin', admin);
-
-
+app.use('/login', login);
 
 
 app.listen(3000, () => {
--- controllers/admin.js
+++ controllers/admin.js
@@ -1,8 +1,23 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/home', (req, res) => {
+router.get('/home', isLoggedIn, (req, res) => {
     res.render('admin/home', { title: 'Options'});
 });
+
+router.get('/logout', function(req, res) {
+    req.logout();
+    res.redirect('/');
+});
+
+function isLoggedIn(req, res, next) {
+
+    // if user is authenticated in the session, carry on 
+    if (req.isAuthenticated()&&req.user.usertype=='admin')
+        {return next();}
+
+    // if they aren't redirect them to the home page
+    res.redirect('/');
+}
 
 module.exports = router;
